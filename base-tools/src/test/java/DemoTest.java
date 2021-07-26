18ed1fe85c1bdffd4827a3bdc24c60efdd76256c
AsioSir <3235254126@qq.com>
2021-07-26 21:38:58 +0800

    DemoTest

---
+++ base-tools/src/test/java/DemoTest.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: asio
+ * @time: 2021-07-26 16:42
+ * 描述:
+ */
+public class DemoTest {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("JAVA");
+        list.add("C++");
+        System.out.println(list.toString());
+    }
+
+}
