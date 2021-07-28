2590be8fdd320827122b665f3b74de6d4e02f18e
AsioSir <3235254126@qq.com>
2021-07-28 21:53:58 +0800

    项目配置

--- base-tools/src/main/java/com/asio/tools/BaseToolsApplication.java
+++ base-tools/src/main/java/com/asio/tools/BaseToolsApplication.java
@@ -1,5 +1,14 @@
 package com.asio.tools;
 
-public class BaseToolsApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+//扫描mybatis生成的mapper文件
+//@MapperScan({"com.asio.tools.*"})
+public class BaseToolsApplication {
+    public static void main(String[] args) {
+        //启动程序
+        SpringApplication.run(BaseToolsApplication.class, args);
+    }
 }
